030796d92539d1e070e842fad2a5ce3d0b504967
ITPROVN86 <quangltn3@fe.edu.vn>
2023-05-25 11:35:37 +0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import controller.FruitController;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new FruitController().run();
+    }
+}
